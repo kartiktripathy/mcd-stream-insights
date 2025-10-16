628513842105302d1384e3ca1e010720285ecdb0
Kartik Tripathi <43439398+kartiktripathy@users.noreply.github.com>
2025-10-16 22:13:30 +0530

    Update ksql_db_commands.sql

--- ksql_db_commands.sql
+++ ksql_db_commands.sql
@@ -1,13 +1,13 @@
 -- Orders Stream
 
-CREATE STREAM macd_orders_stream (
+CREATE STREAM mcd_orders_stream (
     order_id STRING,
     customer_id STRING,
     order_total DOUBLE,
     order_items ARRAY<STRUCT<item_name STRING, quantity INT, price DOUBLE>>,
     order_time TIMESTAMP
 ) WITH (
-  KAFKA_TOPIC = 'macd_orders',
+  KAFKA_TOPIC = 'mcd_orders',
     KEY_FORMAT = 'KAFKA',
     VALUE_FORMAT = 'AVRO',
     TIMESTAMP = 'order_time'
@@ -15,14 +15,14 @@ CREATE STREAM macd_orders_stream (
 
 -- Payments stream
 
-CREATE STREAM macd_payments_stream (
+CREATE STREAM mcd_payments_stream (
     payment_id STRING,
     order_id STRING,
     payment_amount DOUBLE,
     payment_method STRING,
     payment_time TIMESTAMP
 ) WITH (
-    KAFKA_TOPIC = 'macd_payments',
+    KAFKA_TOPIC = 'mcd_payments',
     KEY_FORMAT = 'KAFKA',
     VALUE_FORMAT = 'AVRO',
     TIMESTAMP = 'payment_time'
@@ -30,7 +30,7 @@ CREATE STREAM macd_payments_stream (
 
 -- Join stream
 
-CREATE STREAM maacd_orders_payments_joined AS
+CREATE STREAM mcd_orders_payments_joined AS
 SELECT 
     o.order_id AS order_id,
     o.customer_id AS customer_id,
@@ -41,8 +41,8 @@ SELECT
     p.payment_amount AS payment_amount,
     p.payment_method AS payment_method,
     p.payment_time AS payment_time
-FROM macd_orders_stream o
-INNER JOIN macd_payments_stream p
+FROM mcd_orders_stream o
+INNER JOIN mcd_payments_stream p
     WITHIN 24 HOURS
     ON o.order_id = p.order_id
 EMIT CHANGES;
